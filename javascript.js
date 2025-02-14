0b804a3f05507ce9ee222b3182672011eb3931fb
Agrus <davidowart2000pro@gmail.com>
2025-02-14 10:28:21 +0000

    js tasks

---
+++ javascript.js
@@ -0,0 +1,57 @@
+// let name = "John";
+// let surname = "Doe";
+
+// console.log(name);
+// console.log(surname);
+
+// let age = 11;
+// console.log(age);
+
+// age = 54;
+
+// console.log(age);
+
+// const pi = 3.14;
+
+// console.log(pi);
+
+// console.log(23+97)
+
+// console.log(23+32+32/(2*2))
+
+// let a = 19;
+
+// console.log(a);
+
+// a = 2
+
+// console.log(a);
+
+// let b = 7 * a;
+
+// console.log(b);
+
+// const max = 57;
+
+// const actual = max - 13;
+
+// const percentage = actual / max;
+
+// console.log(percentage);
+
+// alert("Welcome")
+
+/** Tasks */
+
+// let a = 1, b = 1;
+
+// let c = ++a;
+// let d = b++;
+
+// console.log(a, b);
+
+
+// let a1 = prompt("First number?");
+// let b2 = prompt("Second number?");
+
+// alert(a1 + b2);
